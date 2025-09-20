a1326bb45b7032aeb8f01ce9819f8b8033324cda
bikash5750 <gbikash5750@gmail.com>
2025-09-20 13:41:46 +0530

    done with cors

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import express from "express";
 import connectdb from "./src/dbconnection/connectdb.js";
 import studentRouter from "./src/routes/student.routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use(cors()); 
 
 app.use("/api/student", studentRouter);
 const startServer = async () => {
