68e26b2e7e85061f3116a6967972b6ce538a7fe6
bikash5750 <gbikash5750@gmail.com>
2025-09-21 19:34:13 +0530

    sliding window ratelimiter done

--- src/middleware/ratelimiter.js
+++ src/middleware/ratelimiter.js
@@ -1,19 +1,36 @@
 import redisclient from "../redisconfig/redis.js";
 
+const windowsize = 3600; //1 hour
+const maxerequests = 60; //60 requests per hour
 const ratelimiter = async (req, res, next) => {
   try {
-    const ip = req.ip; 
+      const key = req.ip;
+      const currentime = Date.now()/1000;
+      const windowtime =  currentime - windowsize;
 
-    //here ip will act as key and incr will be done automatically if key is not present it will create one and set value to 1
-    const count = await redisclient.incr(ip);
+      await redisclient.zRemRangeByScore(key , 0 , windowtime);
 
-    if (count === 1) {
-      await redisclient.expire(ip, 3600);
-    }
+      const numberofrequest = await redisclient.zCard(key);
 
-    if (count > 60) {
-      return res.status(429).json({ message: "Too many requests. Try again later." });
-    }
+      if(numberofrequest >= maxerequests){
+        return res.status(429).json({ message: "Too many requests. Try again later." });
+      }
+
+      await redisclient.zAdd(key , {score:currentime , value : `${currentime}:${Math.random()}`});
+      await redisclient.expire(key , windowsize);
+
+    // const ip = req.ip; 
+
+    // //here ip will act as key and incr will be done automatically if key is not present it will create one and set value to 1
+    // const count = await redisclient.incr(ip);
+
+    // if (count === 1) {
+    //   await redisclient.expire(ip, 3600);
+    // }
+
+    // if (count > 60) {
+    //   return res.status(429).json({ message: "Too many requests. Try again later." });
+    // }
 
     next(); 
   } catch (error) {
